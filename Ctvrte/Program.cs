666132e08f2f73997d9b062646212f623da6c09f
Jiri Falta <jirkafalta@gmail.com>
2020-02-26 08:48:05 +0100

    4. edit

--- Ctvrte/Program.cs
+++ Ctvrte/Program.cs
@@ -19,17 +19,20 @@ namespace Ctvrte
 			var connector = new MainConnector();
 			var min = -4;
 			var max = 8;
-			for (int i = min; i <= max; i++) {
+			Console.WriteLine("Prvni");
+			for (double i = min; i <= max; i += 0.1) {
 				var f = await connector.GetModel(FuncName.funcF, i);
 				var g = await connector.GetModel(FuncName.funcG, i);
 
 				double diff = f.yVal - g.yVal;
-				if (diff > 0) {
+				if (Math.Abs(diff) < 2) {
 					Console.WriteLine($"Pro x = {i} má funce G nižší hodnotu než fce F, a to o {diff}");
 				}
 				await Task.Delay(10);
 			}
-
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine("Druhe");
 			for (double i = min; i <= max; i += 0.25) {
 				var g = await connector.GetModel(FuncName.funcG, i);
 				if (Math.Abs(g.yVal) < 0.001) {
